600c851f53bde9f2e938d5e0d620e4ff8c2f6967
mdominguezd <martin.dominguez.duran@gmail.com>
2024-02-22 11:48:09 -0500

    changed path

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import pathlib
 
 import os
 
-print(os.getcwd())
+print('PATH IS::: ', os.getcwd())
 
 # This adds the path of the …/src folder
 # to the PYTHONPATH variable
 sys.path.append("/mount/src/cva_app/")
 
-from Models import U_Net, U_Net_DANN
+# from Models import U_Net, U_Net_DANN
 from get_image import get_image, crop_image
 from DL_backend import Img_Dataset, predict_cashew
 
